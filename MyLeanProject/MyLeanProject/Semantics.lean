00f529fa019d0bd283a1182c264165cef72bde77
pleungeenen <pleungeenen@gmail.com>
2026-01-26 17:37:11 +0100

    edit 26-01

--- MyLeanProject/MyLeanProject/Semantics.lean
+++ MyLeanProject/MyLeanProject/Semantics.lean
@@ -246,19 +246,9 @@ induction C with
    exact ih1
    exact ih2
 
-/-de 2 var lemma's zeggen dat variabelen niet veranderen
-bij toepassen van auxiliary of duality2-/
-lemma auxiliary_variable (A : PropForm) : (auxiliary A).vars = A.vars := by
-  sorry
-
-lemma duality_variable (A : PropForm) : (duality2 A).vars = A.vars := by
-  sorry
-
 /-A <-> B dan ook auxiliary A <-> auxiliary B-/
 lemma auxiliary_biImpl (A B : PropForm) : (⊨ biImpl A B) -> (⊨biImpl (auxiliary A) (auxiliary B)) := by
   sorry
-/-deze 3 niet meer nodig denk ik^^^-/
-
 
 /-auxiliary verandert true in false en andersom-/
 @[simp]
@@ -267,9 +257,12 @@ lemma aux_tr : auxiliary tr = fls := rfl
 @[simp]
 lemma aux_fls : auxiliary fls = tr := rfl
 
-/-hoe moet deze eerste?-/
 @[simp]
-lemma vars_biImpl (A B : PropForm) : (biImpl A B).vars = A.vars && B.vars := by
+lemma vars_biImpl (A B : PropForm) : (biImpl A B).vars = A.vars ∪ B.vars := by
+sorry
+
+@[simp]
+lemma vars_conj (A B : PropForm) : (conj A B).vars = A.vars ∪ B.vars := by
 sorry
 
 /-ze zeggen allemaal dat de variabelen hetzelfde blijven bij omzetting-/
@@ -299,9 +292,19 @@ theorem simultaneous_substitution_theorem (A B : PropForm) (s : List (String × 
   sorry
 
 /-φ∗[¬p0,...,¬pn/p0,...,pn] = φd[¬¬p0,...,¬¬pn/p0,...,pn] uit van dalen boek-/
+@[simp]
 lemma auxiliary_dualitynegation (A : PropForm) : simultaneous_substitution (A.vars.map (fun s => (s, neg (var s)))) (auxiliary A) =
     simultaneous_substitution (A.vars.map (fun s => (s, neg (neg (var s))))) (duality2 A) := by
   sorry
+
+
+/-By the Substitution Theorem |  φd ↔ φ∗[¬p0, . . . , ¬pn/p0, . . . , pn].-/
+lemma substitution_duality_lemma (A : PropForm) :
+    ⊨ biImpl (duality2 A) (simultaneous_substitution (A.vars.map (fun s => (s, neg (var s)))) (auxiliary A)) := by
+      have step_1 : ⊨ biImpl (duality2 A) (simultaneous_substitution (A.vars.map (fun s => (s, neg (neg (var s))))) (duality2 A)) := by
+        sorry
+      simp
+      grind
 
 /-2 x duality toepassen = origineel-/
 lemma duality_involutory (A : PropForm) (v : PropAssignment) :
@@ -316,34 +319,82 @@ lemma duality_involutory (A : PropForm) (v : PropAssignment) :
   | impl A1 A2 ih1 ih2 => simp [duality2, PropForm.eval, ih1, ih2]
   | biImpl A1 A2 ih1 ih2 => simp [duality2, PropForm.eval, ih1, ih2]
 
-lemma duality2_biImpl (A B : PropForm) : (⊨ biImpl A B) → (⊨ biImpl (duality2 A) (duality2 B)) := by
+/-als A<-> B en B<-> C dan A<->C-/
+lemma biImpl_connection (A B C : PropForm) :
+    (⊨ biImpl A B) → (⊨ biImpl B C) → (⊨ biImpl A C) := by
+  sorry
+/-als A<->B dan B<->A-/
+lemma biImpl_symmetry (A B : PropForm) :
+    (⊨ biImpl A B) → (⊨ biImpl B A) := by
+  sorry
+/-duality_involutory als met ⊨ teken-/
+lemma duality_involutory_valid (A : PropForm) :
+    ⊨ biImpl (duality2 (duality2 A)) A := by
+  sorry
+
+  /-hier moeten nog de substitution stappen bij-/
+lemma auxiliary_to_duality (A B : PropForm) :
+    (⊨ biImpl (auxiliary A) (auxiliary B)) → (⊨ biImpl (duality2 A) (duality2 B)) := by
+    intro h
+    /-φ∗ = φd [¬p0, . . . , ¬pn/p0, . . . , pn] de eerste 2 stappen is de regel voor beide formules-/
+    have step1 : auxiliary A = simultaneous_substitution (A.vars.map (fun s => (s, neg (var s)))) (duality2 A) :=
+    auxiliary_lemma A
+    have step2 : auxiliary B = simultaneous_substitution (B.vars.map (fun s => (s, neg (var s)))) (duality2 B) :=
+    auxiliary_lemma B
+    /- /-By the Substitution Theorem |  φd ↔ φ∗[¬p0, . . . , ¬pn/p0, . . . , pn].-/ -/
+    have step3 : ⊨ biImpl (duality2 A) (simultaneous_substitution (A.vars.map (fun s => (s, neg (var s)))) (auxiliary A)) :=
+    substitution_duality_lemma A
+    have step4 : ⊨ biImpl (duality2 B) (simultaneous_substitution (B.vars.map (fun s => (s, neg (var s)))) (auxiliary B)) :=
+    substitution_duality_lemma B
+    sorry
+
+
+lemma duality_biImpl (A B : PropForm) : (⊨ biImpl A B) → (⊨ biImpl (duality2 A) (duality2 B)) := by
+  intro h
+  /-Since φ ↔ ψ, also  ¬φ ↔ ¬ψ-/
+  have step1 : ⊨ biImpl (neg A) (neg B) := negation_biImpl A B h
+  /- By Corollary 2.3.12 φ∗ ↔ ¬φ, ψ∗ ↔ ¬ψ-/
+  have step2 : ⊨ biImpl (auxiliary A) (neg A) := auxiliary2 A
+  have step3 : ⊨ biImpl (auxiliary B) (neg B) := auxiliary2 B
+  /-Hence φ∗ ↔ ψ∗ in de volgende stappen:
+     met biimpl_symmetry kunnen we van negatieB <-> auxiliaryB-/
+  have step4 : ⊨ biImpl (neg B) (auxiliary B) := biImpl_symmetry (auxiliary B) (neg B) step3
+  /- willen dat negatie A <-> auxiliary B,
+      negatie A <-> negatie B, negatie B <-> auxiliary B, met transitiviteit van negatie A <-> auxiliary B-/
+  have step5 : ⊨ biImpl (neg A) (auxiliary B) := biImpl_connection (neg A) (neg B) (auxiliary B) step1 step4
+  /- auxiliary A <-> negatie A, negatie A <-> auxiliary B, met transitiviteit auxiliary A <-> auxiliary B -/
+  have step6 : ⊨ biImpl (auxiliary A) (auxiliary B) := biImpl_connection (auxiliary A) (neg A) (auxiliary B) step2 step5
+  /- dit gaat van auxiliary A <-> auxiliary B naar duality A <-> duality B-/
+  exact auxiliary_to_duality A B step6
+
+/- omgekeerde richting, als dualityA <-> dualityB dan A <->B-/
+lemma duality_biImpl_reversed (A B : PropForm) : (⊨ biImpl (duality2 A) (duality2 B)) → (⊨ biImpl A B) := by
+  intro h
+  /- als dualityA <-> dualityB dan met duality2_biimpl duality duality A <-> duality duality B-/
+  have step1 : ⊨ biImpl (duality2 (duality2 A)) (duality2 (duality2 B)) := duality_biImpl (duality2 A) (duality2 B) h
+  /- duality 2 keer toepassen geeft de originele formule terug, dualitydualityA <-> A-/
+  have step2 : ⊨ biImpl (duality2 (duality2 A)) A := duality_involutory_valid A
+  have step3 : ⊨ biImpl (duality2 (duality2 B)) B := duality_involutory_valid B
+  /- A <-> dualitydualityA-/
+  have step4 : ⊨ biImpl A (duality2 (duality2 A)) := biImpl_symmetry (duality2 (duality2 A)) A step2
+  /- A <-> dualitydualityA en dualitydualityA <-> dualitydualityB, wordt A <->dualitydualityB-/
+  have step5 : ⊨ biImpl A (duality2 (duality2 B)) := biImpl_connection A (duality2 (duality2 A)) (duality2 (duality2 B)) step4 step1
+  /- A <-> dualitydualityB met dualitydualityB <-> B wordt A <-> B-/
+  exact biImpl_connection A (duality2 (duality2 B)) B step5 step3
+
+/-ik heb nog een andere substitution nodig denk ik? als ik het volgens het boek wil doen-/
+/-twee cases, -> en <-?-/
+theorem duality_theorem (A B : PropForm) : (⊨ (biImpl A B)) ↔ (⊨ (biImpl (duality2 A) (duality2 B))) := by
+  constructor
+  · intro h
+    exact duality_biImpl A B h
+  · intro h
+    exact duality_biImpl_reversed A B h
+
+/-lemma duality2_biImpl (A B : PropForm) : (⊨ biImpl A B) → (⊨ biImpl (duality2 A) (duality2 B)) := by
   intro h
   simp [PropForm.isValid] at *
   intro a
   sorry
-
 lemma duality2_biImpl_reversed (A B : PropForm) : (⊨ biImpl (duality2 A) (duality2 B)) → (⊨ biImpl A B) := by
-  sorry
-
-theorem duality_theorem (A B : PropForm) : (⊨ (biImpl A B)) ↔ (⊨ (biImpl (duality2 A) (duality2 B))) := by
-  constructor
-  intro h
-  /-de volgorde moet zo denk ik maar ik weet niet precies hoe ik het moet toepassen:
-  negation_biImpl
-  auxiliary2
-  simultaneous_substitution
-  auxiliary_dualitynegation
-  duality_involutory-/
-  sorry
-
-
-
-/-ik heb nog een andere substitution nodig denk ik? als ik het volgens het boek wil doen-/
-/-twee cases, -> en <-?-/
-/-theorem duality_theorem (A B : PropForm) : (⊨ (biImpl A B)) ↔ (⊨ (biImpl (duality2 A) (duality2 B))) := by
-  constructor
-  · intro h
-    exact duality2_biImpl A B h
-  · intro h
-    exact duality2_biImpl_reversed A B h
--/
+  sorry-/
