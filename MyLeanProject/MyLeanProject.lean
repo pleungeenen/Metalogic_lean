4c85b6959b288efb97db9320f833fa7576c3eb8f
pleungeenen <pleungeenen@gmail.com>
2026-01-29 15:13:23 +0100

    verwijder oud project

--- MyLeanProject/MyLeanProject.lean
+++
@@ -1,5 +0,0 @@
--- This module serves as the root of the `MyLeanProject` library.
--- Import modules here that should be built as part of the library.
-import MyLeanProject.Basic
-import MyLeanProject.Semantics
-import MyLeanProject.NormalForms
